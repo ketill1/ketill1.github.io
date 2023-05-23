301fd14bf32b0bfd25c59681010cfedac2dbf20e
ketill1 <42077535+ketill1@users.noreply.github.com>
2023-05-23 23:38:10 +0200

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,1 +1,28 @@
 
+import turtle
+
+def draw_fish():
+    window = turtle.Screen()
+    window.bgcolor("white")
+
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("blue")
+    brad.speed(1)
+
+    # draw the body of fish
+    brad.circle(50)
+
+    # draw the tail of fish
+    brad.right(60)
+    brad.forward(100)
+    brad.right(120)
+    brad.forward(100)
+
+    # hide the turtle
+    brad.hideturtle()
+
+    # close on click
+    window.exitonclick()
+
+draw_fish()
